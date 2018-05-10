d9f8a7146bb9d7ccca7e010f9036c699a9676f3b
Charlie Garnaas <cgarnaas555@gmail.com>
2018-05-10 09:01:47 -0500

    New Component Addition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from '../api/products/product-list.component';
+import { NewcomponentComponent } from './newcomponent/newcomponent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    NewcomponentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/newcomponent/newcomponent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pm-newcomponent',
+  templateUrl: './newcomponent.component.html',
+  styleUrls: ['./newcomponent.component.css']
+})
+export class NewcomponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
